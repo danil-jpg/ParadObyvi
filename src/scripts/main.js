78e4aa00b501e4261262fabae60885152fb9cf41
WeeRbe <danil.aliev97@gmail.com>
2022-11-16 12:35:37 +0200

    header-mobyle

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 import Swiper from 'swiper'
-import mobileMenu from './modules/footer.js'
 import header from './modules/header.js';
+import mobileMenu from './modules/footer.js'
 
 mobileMenu()
 header()
+
--- src/scripts/modules/header.js
+++ src/scripts/modules/header.js
@@ -1,9 +1,9 @@
 function header () {
     const headerProblemClose = document.querySelector('.header-problem__close');
     const headerProblem = document.querySelector('.header-problem');
-
+    
     headerProblemClose.addEventListener('click', () => {
-        headerProblem.classList.add('close')
+        headerProblem.classList.toggle('close')
     })
 }
 
