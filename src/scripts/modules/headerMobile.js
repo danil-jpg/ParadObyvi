9586176fc78e819cac7c86b51560b0265cab1353
Danil <danil.polovinkin.1994@gmail.com>
2022-11-16 16:37:18 +0300

    completed burgers

--- src/scripts/modules/headerMobile.js
+++ src/scripts/modules/headerMobile.js
@@ -1,7 +1,9 @@
 function mobileBurger () {
   const menuTrigger1 = document.querySelector('.menu-trigger_1')
+  const menuTrigger2 = document.querySelector('.menu-trigger_2')
   const menusImg = document.querySelector('.trigger-img_pc')
   const menu = document.querySelector('.header__mobile-menu')
+  const menu2 = document.querySelector('.mobile-menu_2')
 
   menuTrigger1.addEventListener('click', () => {
     menuTrigger1.classList.toggle('active')
@@ -14,6 +16,11 @@ function mobileBurger () {
     }
 
     menu.classList.toggle('active')
+  })
+
+  menuTrigger2.addEventListener('click', () => {
+    menuTrigger2.classList.toggle('active')
+    menu2.classList.toggle('active')
   })
 }
 
